2b5efd8be1517dbb4df18a6840ca6b7562a122c0
grinvolod13 <grinvols@gmail.com>
2023-08-06 03:35:47 +0300

    add @abstractmethod

--- core/data/repository.py
+++ core/data/repository.py
@@ -1,7 +1,7 @@
 from copy import copy, deepcopy
 from core.data import Repository
 from core.domain.model import User
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 
@@ -9,18 +9,23 @@ class UserRepositoryAbstract(Repository, ABC): # pragma: no cover
     def __init__(self, db):
         self.db = db
         
+    @abstractmethod
     def get(self, username) -> User | None:
         raise NotImplementedError()
-        
+    
+    @abstractmethod
     def add(self, user: User) -> User | None:
         raise NotImplementedError()
     
+    @abstractmethod
     def remove(self, user: User):
         raise NotImplementedError()
     
+    @abstractmethod
     def get_by_email(self, email) -> User | None:
         raise NotImplementedError()
     
+    @abstractmethod
     def exists(self, user: User) -> bool:
         raise NotImplementedError()
     
@@ -55,3 +60,6 @@ class UserRepositoryTest(UserRepositoryAbstract):
         if search:
             return copy(search[0])
         return None
+
+    
+    
