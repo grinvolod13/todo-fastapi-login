a598545f978d00af7aab9c1aa66502d4f29357a8
grinvolod13 <grinvols@gmail.com>
2023-08-06 05:57:44 +0300

    add register api

--- core/data/dependency.py
+++ core/data/dependency.py
@@ -1,3 +1,15 @@
 from dotenv import dotenv_values
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
 
 config = dotenv_values(".env")
+
+engine = create_engine(config['CONNECTION_STRING'])
+
+def get_db():
+    with sessionmaker(engine)() as db:
+        yield db
+        db.commit()
+    
+
--- features/register/api.py
+++ features/register/api.py
@@ -1,19 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from core.domain.model import User
 from .domain.usecases import RegisterUserUseCase
+import core.data.dependency as dependency
 
-
-from core.data.repository import UserRepositoryTest
+from core.data.repository import UserRepositorySqlalchemy
 
 
 router = APIRouter()
 
-repo = UserRepositoryTest([])
 
 @router.post("/register")
-def register(user: User):
+def register(user: User, db = Depends(dependency.get_db)):
+    repo = UserRepositorySqlalchemy(db)
     return RegisterUserUseCase(repo).execute(user)
 
 @router.get("/register")
-def list_users():
-    return repo.db
+def list_users(db = Depends(dependency.get_db)):
+    repo = UserRepositorySqlalchemy(db)
+    return repo.db.execute(select(User)).scalars().fetchall()
