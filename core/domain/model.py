f568dacad5575c9bd502572d6a776340c1bf647e
grinvolod13 <grinvols@gmail.com>
2023-08-06 17:47:35 +0300

    add User __eq__ to username&pass comparsion

--- core/domain/model.py
+++ core/domain/model.py
@@ -12,5 +12,8 @@ class User():
         self.email = email
         self.password = security.generate_password_hash(password)
 
-        
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, User):
+            return False
+        return self.username == __value.username and self.email == __value.email
     
